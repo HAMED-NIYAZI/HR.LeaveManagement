22c0bd2e5f0a77e0ca335d9f632022fa12a0b0cb
USER <USER@DESKTOP-20DRNS1>
2023-02-01 00:39:21 +0330

    15 add mapping and refactor

--- HR.LeaveManagement.Application/Features/LeaveAllocations/Requests/Queries/GetLeaveAllocationListRequest.cs
+++ HR.LeaveManagement.Application/Features/LeaveAllocations/Requests/Queries/GetLeaveAllocationListRequest.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace HR.LeaveManagement.Application.Features.LeaveAllocations.Requests.Queries
 {
-    internal class GetLeaveAllocationListRequest:IRequest<List<CreateLeaveAllocationDto>>
+    public class GetLeaveAllocationListRequest:IRequest<List<CreateLeaveAllocationDto>>
     {
     }
 }
