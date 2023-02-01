8cb30f02dbd8f43a4ed8ad914597c128fd5f5652
USER <USER@DESKTOP-20DRNS1>
2023-02-01 22:09:58 +0330

    Add Ef core and configure it

---
+++ HR.LeaveManagement.Persistence/HrLeaveManagementDbContext.cs
@@ -0,0 +1,40 @@
+﻿ using HR.LeaveManagement.Domain.Entity;
+using HR.LeaveManagement.Domain.Entity.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public class LeaveManagementDbContext : DbContext
+    {
+        public LeaveManagementDbContext(DbContextOptions<LeaveManagementDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveManagementDbContext).Assembly);   
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken=default)
+        {
+
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())//چنج ترک ها را نگاه میکند
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;         //آخرین اپدیت را ست میکند
+                if (entry.State==EntityState.Added)                 //اگر وضعیت ادد بود تاریخ ایجاد را تاریخ جاری میزند
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    }
+}
