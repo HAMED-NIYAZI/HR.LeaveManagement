b411aad7a8b471bfb9a4603e06567434b33d3dcf
USER <USER@DESKTOP-20DRNS1>
2023-02-03 01:15:25 +0330

    30 Add Unit Test

---
+++ HR.LeaveManagement.Application.UnitTests/LeaveTypes/Commands/CreateLeaveTypeCommandHandlerTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.DTOs.LeaveType;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Commands;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
+using HR.LeaveManagement.Application.Profiles;
+using HR.LeaveManagement.Application.UnitTests.Mocks;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HR.LeaveManagement.Application.UnitTests.LeaveTypes.Commands
+{
+    public class CreateLeaveTypeCommandHandlerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ILeaveTypeRepository> _mockRepo;
+        private readonly CreateLeaveTypeDto _leaveTypeDto;
+        public CreateLeaveTypeCommandHandlerTests()
+        {
+            _mockRepo = MockLeaveTypeRepository.GetLeaveTypeRepositories();
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
+
+            _mapper = new Mapper(mapperConfig);
+
+            _leaveTypeDto = new CreateLeaveTypeDto
+            {
+                DefaultDays = 15,
+                Name = "Test Dto"
+            };
+        }
+
+        [Fact]
+        public async Task CreateLeaveType()
+        {
+            var handler = new CreateLeaveTypeCommandHandler(_mockRepo.Object, _mapper);
+            var result = await handler.Handle(new CreateLeaveTypeCommand() { LeaveTypeDto = _leaveTypeDto }, CancellationToken.None);
+
+
+
+
+        }
+    }
+}
