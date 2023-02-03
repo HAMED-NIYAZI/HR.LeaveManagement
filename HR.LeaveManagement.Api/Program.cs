e50754f7bbafd494f1926dbc9231127a27432f2f
USER <USER@DESKTOP-20DRNS1>
2023-02-03 15:18:11 +0330

    28 submited

--- HR.LeaveManagement.Api/Program.cs
+++ HR.LeaveManagement.Api/Program.cs
@@ -40,9 +40,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c=>c.SwaggerEndpoint("/swagger/v1/swagger.json","HR.LeaveManagement.Api v1"));
+    app.UseDeveloperExceptionPage();
+   // app.UseSwagger();
+   // app.UseSwaggerUI(c=>c.SwaggerEndpoint("/swagger/v1/swagger.json","HR.LeaveManagement.Api v1"));
 }
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HR.LeaveManagement.Api v1"));
 
 app.UseHttpsRedirection();
 app.UseRouting();
